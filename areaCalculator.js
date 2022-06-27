a2a0e092246a5393747bf8901d6aaae2d6e5a7dc
vagrant <h.palomerabecerra@gmail.com>
2022-06-27 19:28:53 +0000

    get area challenge

---
+++ areaCalculator.js
@@ -0,0 +1,29 @@
+const calculateRectangleArea = (length, width) => {
+  let area = ''
+  area = length * width
+  if(length < 0 || width < 0){ return undefined }
+  else{ return area }
+}
+const calculateTriangleArea = (base, height) => {
+  let area = ''
+  area = base * height / 2
+  if(base < 0 || base < 0){ return undefined }
+  else { return area }
+}
+const calculateCircleArea = (radius) => {
+  let area = ''
+  area = Math.PI * radius * radius
+  if(radius < 0){ return undefined }
+  else{ return area }
+}
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
