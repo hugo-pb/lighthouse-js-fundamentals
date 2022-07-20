643a4261c6f17db63ba2e1c17b7a64ed7f19fe07
hugo-pb <h.palomerabecerra@gmail.com>
2022-07-20 14:59:27 -0600

    while loop added

--- challenges/packing-kist.js
+++ challenges/packing-kist.js
@@ -3,6 +3,11 @@ const packingList = ['bowls', 'plates', 'pots', 'pans',
 
 
 console.log('kitchen stuff to pack:');
-for (let i = 0; i < packingList.length; i++){
+// for (let i = 0; i < packingList.length; i++){
+//   console.log(packingList[i]);
+// }
+let i = 0;
+while(i < packingList.length){
   console.log(packingList[i]);
+  i++;
 }
