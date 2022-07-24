06ceab1464f7d4b42a8692141555b07f0bfe3145
hugo-pb <h.palomerabecerra@gmail.com>
2022-07-24 15:49:53 -0600

    solved last index of

--- challenges/lastindexof.js
+++ challenges/lastindexof.js
@@ -1,13 +1,16 @@
 // When this function is given an array and a value, it should return the index of the last time the value occurs in the array. If the value never occurs, the function should return -1.
 
 const lastIndexOf = (array, n) => {
-for(let i = 0; i < array.length; i++){
-  if(array[i] == n){    console.log('found')}
-  else{console.log('not found')}
+  let finalidex = '';
+for(let i = array.length; i > -1; i--){
+  if(array[i] == n){ 
+  
+  finalidex = i;
+  break;
+}
+  else {finalidex = -1 ;}
+}return(finalidex)
+}
 
-   
- 
-}
-}
 
 lastIndexOf([1,2,3,4,4], 4)
