e6d2c3bf62e1d1d54f2bc30f19b88e63c59880c5
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-10 09:08:52 -0600

    log solution insted of return

--- challenges/loopyLighthouse2.js
+++ challenges/loopyLighthouse2.js
@@ -1,17 +1,15 @@
 
 let loopyLighthouse = (range, multiples, words) => {
-  n = '';
-for(let i = range[0]; i <= range[1]; i++){
- if(i % 2 === 0 && i % 5 === 0){
-  n += 'BattyBeacon '
- }else if(i % 2 ===0) {
-  n += 'Batty '
- }else if(i % 5 ===0) {
-  n += 'Beacon '
- }else {
-n += i + ' '
+  for(let i = range[0]; i <= range[1]; i++){
+    if(i % multiples[0] === 0 && i % multiples[1] === 0){
+    console.log(words[0] + words[1])
+ }  else if(i % multiples[0] === 0) {
+  console.log(words[0])
+ }  else if(i % multiples[1] === 0) {
+  console.log(words[1])
+ }  else {
+console.log(i) }
  }
-}return n;
 }
 
 console.log(loopyLighthouse([15, 50], [2, 5], ["Batty", "Beacon"]));
