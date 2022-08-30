6ea6f8927b70ed54e2578e33e523d9c80de36b56
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-29 19:57:07 -0600

    code refactored

--- challenges/Title.js
+++ challenges/Title.js
@@ -2,19 +2,24 @@ const titleCase = str => {
   if (!str.length) {
     return "";
   }
-  let newstr = "";
-  for (let i = 0; i < str.length; i++) {
-    if (i === 0) {
-      newstr = str[i].toUpperCase();
-      continue;
-    }
-    if (str[i - 1] === " ") {
-      newstr += str[i].toUpperCase();
-      continue;
-    }
-    newstr += str[i].toLowerCase();
+  newstr = str.toLowerCase().spit(" ");
+  for (let i = 0; i < newstr.length; i++) {
+    newstr[i] = newstr[i].charAt(0).toUpperCase() + newstr[i].slice(1);
   }
-  return newstr;
+  return newstr.join(" ");
+  // let newstr = "";
+  // for (let i = 0; i < str.length; i++) {
+  //   if (i === 0) {
+  //     newstr = str[i].toUpperCase();
+  //     continue;
+  //   }
+  //   if (str[i - 1] === " ") {
+  //     newstr += str[i].toUpperCase();
+  //     continue;
+  //   }
+  //   newstr += str[i].toLowerCase();
+  // }
+  // return newstr;
 };
 
 titleCase("this is an example"); // should return "This Is An Example"
