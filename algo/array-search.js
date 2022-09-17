fd148c947ff6c413c6d48c91eca55b5b7799b24e
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-16 23:33:02 -0600

    binary

--- algo/array-search.js
+++ algo/array-search.js
@@ -11,3 +11,28 @@ function search(array, item) {
 
   return index;
 }
+// binary search
+function search(array, item) {
+  let min = 0;
+  let max = array.length - 1;
+
+  while (true) {
+    const middleIndex = Math.floor((min + max)/2.0);
+    const currentItem = array[middleIndex];
+
+    if (currentItem === item) {
+      // Found it
+      return middleIndex;
+    } else if (currentItem < item) {
+      // Look at the right side
+      min = middleIndex + 1;
+    } else {
+      // Look at the left side
+      max = middleIndex - 1;
+    }
+
+    if (min > max) {
+      return null;
+    }
+  }
+}
